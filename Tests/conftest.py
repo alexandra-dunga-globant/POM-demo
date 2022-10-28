2c635b228e2d7b7defb877bfb6b0883b354c3a20
Alexandra Dunga <alexandra.dunga@globant.com>
2022-10-28 12:12:49 +0300

    Adding BooksPage.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from Config.config import TestData
+from Pages.BooksPage import BooksPage
 from Pages.LoginPage import LoginPage
+
 # TO DO
 #place test files under folders
 
@@ -16,3 +18,9 @@ def setup_home_page(init_driver):
     homePage = loginPage.login(TestData.USERNAME, TestData.PASSWORD)
     yield homePage
     homePage.logout()
+
+@pytest.fixture(scope="class")
+def setup_books_page(init_driver):
+    print("Init_driver driver: ", init_driver)
+    booksPage = BooksPage(init_driver)
+    yield booksPage
