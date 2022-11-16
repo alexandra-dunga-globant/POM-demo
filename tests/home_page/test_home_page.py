5dea86f2e45f32eba96c3c078bd0c19539f7ccfb
Alexandra Dunga <alexandra.dunga@globant.com>
2022-11-16 14:49:58 +0200

    add dependent tests add smoke tests

--- tests/home_page/test_home_page.py
+++ tests/home_page/test_home_page.py
@@ -5,9 +5,17 @@ import pytest
 from config.config import TestData
 from tests.test_base import BaseTest
 
+
 @pytest.mark.env("QA")
 class TestHome(BaseTest):
 
+    @pytest.mark.dependency()
+    @pytest.mark.xfail(reason="deliberate fail")
+    def test_unsuccessfull_login(self, setup_login_page):
+        setup_login_page.login(TestData.USERNAME, TestData.WRONG_PASSWORD)
+        assert False
+
+    @pytest.mark.smoke
     def test_home_page_title(self, setup_home_page):
         title = setup_home_page.get_home_page_title(TestData.HOME_PAGE_TITLE)
         assert title == TestData.HOME_PAGE_TITLE
@@ -17,9 +25,13 @@ class TestHome(BaseTest):
         header = setup_home_page.get_header_value(TestData.HOME_PAGE_HEADER)
         assert header == TestData.HOME_PAGE_HEADER
 
+    @pytest.mark.dependency(depends=['test_unsuccessfull_login'])
+    @pytest.mark.skip(reason="deliberate skip")
     def test_home_page_account_name(self, setup_home_page):
         account_name = setup_home_page.get_account_name_value()
         assert account_name == TestData.ACCOUNT_NAME
 
+    @pytest.mark.dependency(depends=['test_unsuccessfull_login'])
+    @pytest.mark.skip(reason="deliberate skip")
     def test_home_page_logout_button(self, setup_home_page):
         assert setup_home_page.is_logout_displayed()
--- tests/login_page/test_login_page.py
+++ tests/login_page/test_login_page.py
@@ -1,15 +1,18 @@
 __author__ = 'Alexandra Dunga'
 
+import pytest
+
 from config.config import TestData
 from tests.test_base import BaseTest
 
 
 class TestLogin(BaseTest):
-
+    @pytest.mark.smoke
     def test_signup_link_visible(self, setup_login_page):
         flag = setup_login_page.is_signup_link_displayed()
         assert flag
 
+    @pytest.mark.smoke
     def test_login_page_title(self, setup_login_page):
         title = setup_login_page.get_title(TestData.LOGIN_PAGE_TITLE)
         assert title == TestData.LOGIN_PAGE_TITLE
