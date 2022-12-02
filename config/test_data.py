2dd84b055399192efe1d415030b276945aafae28
Alexandra Dunga <alexandra.dunga@globant.com>
2022-12-02 19:50:29 +0200

    remove comments in test_data.py

--- config/test_data.py
+++ config/test_data.py
@@ -1,10 +1,5 @@
 __author__ = 'Alexandra Dunga'
 
-from decouple import config
-
-
-# TO DO
-# to run remote tests on Github actions (including passwords)
 
 class TestData:
     BASE_URL = {
@@ -15,9 +10,11 @@ class TestData:
     WRONG_PASSWORD = "JustATest"
 
     # The values against we do ASSERTS in Test classes
+
     #Login page
     LOGIN_PAGE_TITLE = "ToolsQA"
     LOGIN_PAGE_HEADER = "Login"
+
     #Home page
     HOME_PAGE_TITLE = "ToolsQA"
     HOME_PAGE_HEADER = "Profile"
